50a1419dca43cdd6f5cf20aff75874abb98ffe5b
Jorge <georgevazj@gmail.com>
2016-12-01 10:25:23 +0100

    Completado el dashboard del usuario

--- js/controllers.js
+++ js/controllers.js
@@ -2,5 +2,8 @@ angular.module("bcminer")
   .controller("MainController", function($scope,$rootScope, $http){
     $rootScope.mainTitle = "BlockChain Miner";
     $scope.brand = "Simulador de BlockChain";
-    $scope.left = "Transacciones"
+    $scope.left = "Transacciones actuales";
+    $scope.middle = "Bloque de transacciones"
+    $scope.right = "Controles";
+    $scope.jumbo = "Cadena de bloques";
   })
